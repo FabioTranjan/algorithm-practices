63eefbffc17022ff0bbe0fe97de151c23f08086d
Fabio <fabiotkx@gmail.com>
2021-03-25 00:34:01 -0300

    Implemented algorithm to find first non duplicated character in a string.

---
+++ hashing/find_non_duplicated.rb
@@ -0,0 +1,27 @@
+# Find first non duplicated
+#
+# Basic algorithm to find non-duplicated character in a string
+# Performance: O(N) linear
+
+def find_non_duplicate(string)
+  hash_table = {}
+  splitted_string = string.split('')
+
+  splitted_string.each do |c|
+    if hash_table[c]
+      hash_table[c] += 1
+    else
+      hash_table[c] = 1
+    end
+  end
+
+  splitted_string.each do |c|
+    return c if hash_table[c] == 1
+  end
+
+  return
+end
+
+p find_non_duplicate('minimum')
+p find_non_duplicate('abac')
+p find_non_duplicate('aaa')
