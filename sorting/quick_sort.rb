fbf15de0c33dc424549f2a9b3e05da52e6b8e4ae
Fabio <fabiotkx@gmail.com>
2021-04-02 10:17:02 -0300

    Separate sortable array to be reusable.

--- sorting/quick_sort.rb
+++ sorting/quick_sort.rb
@@ -3,48 +3,19 @@
 # Algorithm to sort an array based on recursion and partitioning
 # Performance: O(N * logN) quadratic
 
+require_relative '../utils/sortable_array'
+
 class QuickSort
-  attr_reader :array
+  attr_reader :sortable_array
 
   def initialize(array)
-    @array = array
-  end
-
-  def partition!(left_pointer, right_pointer)
-    pivot_index = right_pointer
-    pivot = @array[pivot_index]
-
-    right_pointer -= 1
-
-    while true
-      while @array[left_pointer] < pivot do
-        left_pointer += 1
-      end
-
-      while @array[right_pointer] > pivot do
-        right_pointer -= 1
-      end
-
-      if left_pointer >= right_pointer
-        break
-      else
-        @array[left_pointer], @array[right_pointer] =
-          @array[right_pointer], @array[left_pointer]
-
-        left_pointer += 1
-      end
-    end
-
-    @array[left_pointer], @array[pivot_index] =
-      @array[pivot_index], @array[left_pointer]
-
-    return left_pointer
+    @sortable_array = SortableArray.new(array)
   end
 
   def run!(left_index, right_index)
     return if right_index - left_index <= 0
   
-    pivot_index = partition!(left_index, right_index)
+    pivot_index = sortable_array.partition!(left_index, right_index)
 
     run!(left_index, pivot_index - 1)
     run!(pivot_index + 1, right_index)
