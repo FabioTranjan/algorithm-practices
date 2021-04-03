ef6f77248e0fb8ff71124357450dc409d360ebd9
Fabio <fabiotkx@gmail.com>
2021-04-03 16:46:36 -0300

    Added more auxiliary methods.

--- data_structures/linked_list.rb
+++ data_structures/linked_list.rb
@@ -84,6 +84,34 @@ class LinkedList
       current_node = current_node.next_node
     end
   end
+
+  def last
+    last_node = first_node
+    current_node = first_node
+
+    while current_node != nil  do
+      last_node = current_node
+      current_node = current_node.next_node
+    end
+
+    return last_node.data
+  end
+
+  def reverse!
+    previous_node = nil
+    current_node = first_node
+
+    while current_node
+      next_node = current_node.next_node
+
+      current_node.next_node = previous_node
+
+      previous_node = current_node
+      current_node = next_node
+    end
+
+    self.first_node = previous_node
+  end
 end
 
 node_1 = Node.new('once')
@@ -97,4 +125,5 @@ node_3.next_node = node_4
 
 list = LinkedList.new(node_1)
 
+list.reverse!
 list.print
