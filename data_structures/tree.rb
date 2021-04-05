ed3add4a5cbd2de7a97443c441f139bdc614aaef
Fabio <fabiotkx@gmail.com>
2021-04-05 08:19:03 -0300

    Added a BST data structure.

---
+++ data_structures/tree.rb
@@ -0,0 +1,50 @@
+# Tree based data-structures with BST methods
+
+class TreeNode
+  attr_accessor :value, :left, :right
+
+  def initialize(value, left, right)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+  def search(value)
+    return self if self.nil? || self.value == value
+
+    return left.search(value) if value < self.value
+    right.search(value)
+  end
+
+  def insert(value)
+    if value < self.value
+      if left
+        left.insert(value)
+      else
+        self.left = TreeNode.new(value, nil, nil)
+      end
+    elsif value > self.value
+      if right
+        right.insert(value)
+      else
+        self.right = TreeNode.new(value, nil, nil)
+      end
+    end
+  end
+
+  def print
+    return if self.nil?
+
+    left.print unless left.nil?
+    p value
+    right.print unless right.nil?
+  end
+end
+
+left = TreeNode.new(1, nil, nil)
+right = TreeNode.new(3, nil, nil)
+root = TreeNode.new(2, left, right)
+
+p root.search(1)
+root.insert(4)
+root.print
