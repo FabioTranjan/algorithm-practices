c027867e4e9685f8080e20738d94cbb2cf3a384a
Fabio <fabiotkx@gmail.com>
2021-04-11 03:08:05 -0300

    Implemented trie data structure.

---
+++ data_structures/trie.rb
@@ -0,0 +1,49 @@
+require_relative './trie_node'
+
+class Trie
+  def initialize()
+    @root = TrieNode.new
+  end
+
+  def search(word)
+    currentNode = @root
+
+    word.split('').each do |char|
+      return nil unless currentNode.children[char]
+
+      currentNode = currentNode.children[char]
+    end
+
+    return currentNode
+  end
+
+  def insert(word)
+    currentNode = @root
+
+    word.split('').each do |char|
+      if currentNode.children[char]
+        currentNode = currentNode.children[char]
+      else
+        newNode = TrieNode.new
+        currentNode.children[char] = newNode
+        currentNode = newNode
+      end
+    end
+
+    currentNode.children['*'] = nil
+  end
+
+  def collect_all_words(node = nil, word = '', words = [])
+    currentNode = node || @root
+
+    currentNode.children.each do |key, child|
+      if key == '*'
+        words << word
+      else
+        collect_all_words(child, word + key, words)
+      end
+    end
+
+    return words
+  end
+end
